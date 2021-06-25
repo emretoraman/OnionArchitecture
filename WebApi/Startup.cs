f7c72bb29d15f7aa394b3b0a3d2bf987fa5cbdc7
Emre Toraman <emretoraman@gmail.com>
2021-06-25 07:15:57 +0300

    domain entities and application interfaces

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,3 +1,4 @@
+using Application;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,6 +33,8 @@ namespace WebApi
 				c.IncludeXmlComments($@"{AppDomain.CurrentDomain.BaseDirectory}\OnionArchitecture.xml");
 				c.SwaggerDoc("v1", new OpenApiInfo { Title = "OnionArchitecture", Version = "v1" });
 			});
+
+			services.AddApplication();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
